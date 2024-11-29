1ef992128a724c3b448e82335c4df0d63a7353e9
yagna-png <n01697963@humber.ca>
2024-11-28 23:49:31 -0500

    Update1.js
    changes injs

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ const weatherInfoSection = document.querySelector('.weather-info')
 const notFoundSection = document.querySelector('.not-found')
 const searchCitySection = document.querySelector('.search-city')
 
+const countryTxt = document.queryselector('.country-txt')
+const tempTxt = document.querySelector(' .temp-txt')
+const conditionTxt = document.querySelector(' .condition-txt')
+const humidityValueTxt = document.querySelector('.humidity-value-txt')                                            
+const windValueTxt = document.querySelector('.wind-value-txt')
+const weatherSummaryimg = document.querySelector('.weather-summary-img')
+const currentDateTxt = document.querySelector('.current=date-txt')
+
 const apiKay = '2dcae6e892c91d6bf1801ad24bdf9d16'
 
 
@@ -29,11 +37,16 @@ cityInput.addEventListener('keydown', (event) =>{
 })
 
 
-async function getFetchData(endPoint, city){
-    const apiUrl = `https://api.openweathermap.org/data/2.5/${ndPoint}?q=${city}&appid=${apiKey}&units=metric`
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
     const response =  await fetch(apiUrl)
 
     return response.json()
+}
+
+function getWeatherIcon(id) {
+    console.log(id)
+
 }
 
 async function updateWeatherInfo(city){
@@ -44,6 +57,22 @@ async function updateWeatherInfo(city){
         return
     } 
     console.log(weatherData)
+
+    const {
+        name: country,
+        main: { temp, humidity },
+        weather: [{ id, main }],
+        wind: { speed }
+    } = weatherData
+
+    countryTxt.textContent = country
+    tempTxt.textContent = Math.round(temp) + ' degree C '
+    conditionTxt.textContent = main
+    humidityValueTxt.textContent = humidity + ' % '
+    windValueTxt.textContent = speed + ' M/s'
+
+    weatherSummaryImg.src = 'assets/weather/${getWeaatherIcon(id)}
+    
     showDisplaySection(weatherInfoSection)
 }
 function showDisplaySection(section){
